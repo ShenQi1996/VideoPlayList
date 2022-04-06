3fc7016d4eae56f2d9fc91b8ac5a858ab65815ea
ShenQi1996 <qisam1989@gmail.com>
2022-04-06 16:05:57 -0400

    setup done

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+module.exports = {
+  entry: "./frontend/VideoPlayList.jsx",
+  output: {
+    path: path.resolve(__dirname),
+    filename: "./dist/bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env", "@babel/react"],
+          },
+        },
+      },
+    ],
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".jsx", "*"],
+  },
+};
