b2431b613097c5d72443172288943e280d6ef0d7
ShenQi1996 <qisam1989@gmail.com>
2022-04-08 17:52:37 -0400

    working on frontend

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,14 +1,29 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
 
 //Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 const Root = () => {
+    const [videos, setVideos] = useState([]);
+    const [start, setStart] = useState(false)
+    useEffect(() => {
+        fetch("http://localhost:3000/videos")
+        .then(res => res.json())
+        .then(response => {
+            setVideos(response.videos)
+            console.log(videos)
+        })
+    }, [start])
+
+    const handleStart = () =>{
+        setStart(!start)
+    }
+
     return (
         <div>
             <h1>hello </h1>
-            <Button>Start</Button>
+            <Button onClick={handleStart}>Start</Button>
         </div>   
     )
 }
